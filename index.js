f16fff8e3cea47ca0ca1c01dc3d93092684d0399
nedoratic <nedoratic@gmail.com>
2024-02-20 23:40:56 +0100

    logical error corrected

--- index.js
+++ index.js
@@ -191,15 +191,14 @@ const setPlayerCards = (arr = players) => {
 		.map(
 			({ name, position, number, isCaptain, nickname }) =>
 				`
-                    <div class="player-card">
-                        <h2>${name} ${isCaptain ? '(Captain)' : ''}</h2>
-                        <p>Position: ${position}</p>
-                        <p>Number: ${number}</p>
-                        <p>Nickname: ${nickname !== null ? nickname : 'N/A'}</p>
-                    </div>
+                <div class="player-card">
+                    <h2>${name} ${isCaptain ? '(Captain)' : ''}</h2>
+                    <p>Position: ${position}</p>
+                    <p>Number: ${number}</p>
+                    <p>Nickname: ${nickname !== null ? nickname : 'N/A'}</p>
+                </div>
                 `
 		)
-		.toString()
 		.join('');
 };
 
